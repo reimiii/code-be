e2fd60b38d71ccf78cc425a3019082d2dc5a9d7d
Hilmi AM <imiia75775@proton.me>
2024-08-24 12:04:18 +0700

    init

---
+++ src/main/java/code/be/codebe/CodeBeApplication.java
@@ -0,0 +1,13 @@
+package code.be.codebe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/code/be/codebe/CodeBeApplicationTests.java
@@ -0,0 +1,13 @@
+package code.be.codebe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
