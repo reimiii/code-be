7049769e970751b4d416c264bc2a7c2bfcb2f01e
Hilmi AM <imiia75775@proton.me>
2024-08-24 13:07:10 +0700

    update: filtering

---
+++ src/main/java/code/be/codebe/adapters/security/JwtAuthenticationFilter.java
@@ -0,0 +1,30 @@
+package code.be.codebe.adapters.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+  @Override
+  protected void doFilterInternal(
+      HttpServletRequest request,
+      HttpServletResponse response,
+      FilterChain filterChain
+  ) throws ServletException, IOException {
+    final String authHeader = request.getHeader("Authorization");
+    final String jwt;
+    final String userEmail;
+
+    if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+      filterChain.doFilter(request, response);
+      return;
+    }
+
+    jwt = authHeader.substring(7);
+
+  }
+}
